2d00452d73df9fcf92e9593882bf24392270eee5
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-03-22 08:44:24 +0530

    Logic for gambling game

--- Gamble_Machine/script.js
+++ Gamble_Machine/script.js
@@ -11,17 +11,17 @@ const ROWS = 3;
 const COLS = 3;
 
 const SymbolsCount = {
-  'A': 2, //2 A's
-  'B': 4, //4 B's
-  'C': 3,
-  'D': 5,
+  A: 2, //2 A's
+  B: 4, //4 B's
+  C: 3,
+  D: 5,
 };
 const SymbolValues = {
-  'A': 5, // if  user get the line filled with A , he gets 5 multiplier
-  'B': 4,
+  A: 5, // if  user get the line filled with A , he gets 5 multiplier
+  B: 4,
   // if  user get the line filled with B, he gets 4 multiplier
-  'C': 3,
-  'D': 2,
+  C: 3,
+  D: 2,
 };
 
 //Deposit some amount
@@ -56,9 +56,9 @@ const line = () => {
 // Collecting bet
 const bet = (Balance, Lines) => {
   while (true) {
-    const user_bet = prompt("Enter the number of lines between 1-3 ");
+    const user_bet = prompt("Enter the bet per line : ");
     const betnum = parseFloat(user_bet);
-    if (isNaN(betnum) || betnum <= 0 || betnum > Balance / Lines) {
+    if (isNaN(betnum) || betnum <= 0 || betnum > Balance / Lines) {    // bet per line  (if 15 on 2 line then 15*2 < 40 or 15 > 40/2 )
       alert("Invalid Bet");
     } else {
       return betnum;
@@ -75,19 +75,19 @@ const spin = () => {
 
   const wheel = []; //main array that will contain random symbol
   for (let i = 0; i < COLS; i++) {
-    wheel.push([])
+    wheel.push([]);
     const wheelSymbols = [...symbols]; //copy array, this array contains Symbols , each symbol will get removed
     for (let j = 0; j < ROWS; j++) {
       const randomIndex = Math.floor(Math.random() * wheelSymbols.length);
       wheel[i].push(wheelSymbols[randomIndex]); //pusing random symbol to wheel array
-      wheelSymbols.splice(randomIndex, 1);   //remove from available symbols , 1 at a time
+      wheelSymbols.splice(randomIndex, 1); //remove from available symbols , 1 at a time
     }
   }
   return wheel;
 };
 
 const transpose = (reels) => {
-  const rows = [];
+  const rows = []; //array storing the transpose of Symbols
 
   for (let i = 0; i < ROWS; i++) {
     rows.push([]);
@@ -98,41 +98,55 @@ const transpose = (reels) => {
 
   return rows;
 };
-const printRows=(rows)=>{
-    for (const row of rows){
-        let rowString=""
-        for(const [i,symbol] of row.entries())
-        {
-            rowString+=symbol
-            if (i!=rows.length - 1){
-                rowString+= " | "
-            }
+const printRows = (rows) => {
+  for (const row of rows) {
+    let rowString = "";
+    for (const symbol of row) {
+      rowString += `| ${symbol} `;
+      // if (i!=rows.length - 1){
+      //     rowString+= " | "
+      // }
     }
-    console.log(rowString)
-}
-}
-const getWinnings= (rows, bet, lines)=>{
-    let wins=0
-    for(let row= 0 ; row<lines; rows++){
-        const symbols = rows[row]
-        let same = true;
+    console.log(rowString);
+  }
+};
+const getWinnings = (rows, bet, lines) => {
+  let wins = 0;
+  for (let row = 0; row < lines; row++) {
+    const symbols= rows[row];
+    let same = true;
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        same = false;
+        break;
+      }
+    }
+    if (same) {
+      wins += bet * SymbolValues[symbols[0]];
+    }
+  }
+  return wins
+};
 
-        for (const symbol of symbols){
-            if (symbol!= symbols[0]){
-                same=false
-                break
-            }
-        }
-        if (same){
-            wins+=bet * SymbolValues[symbols[0]]
-        }
-    }
-}
-let   Balance = deposit();
+const game=()=>{
+let Balance = deposit();
+while (true) { 
+console.log(`Your current balance is $${Balance}`)
 const NumberOfLines = line();
 const betCollected = bet(Balance, NumberOfLines);
+Balance -= betCollected*NumberOfLines
 const reels = spin();
 const rows = transpose(reels);
-printRows(rows)
-const winner = getWinnings(rows, betCollected ,NumberOfLines)
-console.log("You won , $" + winner.toString())
+
+printRows(rows);
+const winner = getWinnings(rows, betCollected, NumberOfLines);
+Balance+=winner
+console.log("You won , $" + winner.toString());
+if (Balance<=0){
+  console.log("Your ran out of money")
+  break
+}
+const play  = prompt("Do you want to play again ?y/n?")
+if (play != "y") break; 
+}}
+game()
