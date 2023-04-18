18483cfc2bfb657bd3c40bdf58bd2b2fb40e941c
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-04-18 23:43:45 +0530

    Add files via upload

---
+++ Es6 JS/Async_Await/js.js
@@ -0,0 +1,63 @@
+//Async Programming
+//Callback , Promises , Async , Await
+
+const datas = [
+  { name: "Chetan", Profession: "Web_Dev" },
+  { name: "Abhi", Profession: "Web_Dev" },
+];
+
+function getData() {
+  setTimeout(() => {
+    let output = "";
+    datas.forEach((data, index) => {
+      output += `<li>${data.name}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+// Promise function ---->  
+
+const createData = (newdata) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let error = true;
+      if (error) {
+        resolve();
+      } else {
+        reject("Something wrong lagra ");
+      }
+      datas.push(newdata);
+    }, 1000);
+  });
+};
+//Promise function 
+
+
+// createData({name:"Abhay", Profession: "ML engineer" }).then(getData).catch(err => console.log(err))
+
+
+
+
+// Callback function   ---> 
+
+// createData({name:"Abhay", Profession: "ML engineer" },getData)
+// const createData=(newdata,callback)=>{
+//     setTimeout(()=>{
+//         datas.push(newdata)
+//         callback()
+//     },1000)
+// }
+// createData({name:"Abhay", Profession: "ML engineer" },getData)
+
+
+
+
+
+//Async and await   ----->
+
+async function start(){
+    await createData({name:"Abhay", Profession: "ML engineer" })
+    getData()
+}
+start()
