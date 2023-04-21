90aaa5ac1f9ec31c51526887c3271a065eb3e03f
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-04-21 23:54:18 +0530

    Es6 important concepts of Js

---
+++ Es6 JS/Call,Apply,Bind/js.js
@@ -0,0 +1,59 @@
+
+
+//Call()
+// Problem Statement ---->
+// If the function is inside one object and you want to print 2nd object details 
+// let userdetails = {
+//     name:"Chetan Gupta" ,
+//     age :21,
+//     Designation:"Student",
+//     printDetails:function(){
+//         console.log(this.name)
+//     }
+// }
+
+// userdetails.printDetails()
+
+
+// let userdetails2 = {
+//     name:"Gupta" ,
+//     age :21,
+//     Designation:"Engineer",
+// }
+
+
+// //function borrowing
+// userdetails.printDetails.call(userdetails2)
+
+
+
+let userdetails = {
+    name:"Chetan Gupta" ,
+    age :21,
+    Designation:"Student",
+}
+//Function
+let printDetails =function(state,country){
+    console.log(this.name + " " + state+ " "+ country)
+}
+printDetails.call(userdetails , "Jammu","India")
+
+
+let userdetails2 = {
+    name:"Gupta" ,
+    age :21,
+    Designation:"Engineer",
+}
+
+
+//function borrowing-->Call()
+printDetails.call(userdetails2 , "Delhi","India")
+
+//Similar to call()
+//Apply() ---> can pass a list as well
+printDetails.apply(userdetails2 , ["Delhi","India"])
+
+
+//Bind()-----> can create another function by making copy  , call it later 
+let newfun = printDetails.bind(userdetails2 , "Delhi","India")
+newfun()
