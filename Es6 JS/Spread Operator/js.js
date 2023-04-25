8e678d05356fccb222f1d79e6f1d73f2cdcd7dc6
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-04-25 23:33:47 +0530

    Spread and Rest

--- Es6 JS/Spread Operator/js.js
+++ Es6 JS/Spread Operator/js.js
@@ -9,7 +9,7 @@ address={
     state:"jammu",
 }
 
-let a_copy  ={...a,...address}
+let a_copy  ={...a,...address} //using spread 
  a_copy.name = 'Copy' 
 console.log(a,a_copy)   //different name values because its a copy of a not a itself
 
@@ -17,7 +17,7 @@ console.log(a,a_copy)   //different name values because its a copy of a not a it
 
 //Rest Operator
 
-function solve(...num) {
+function solve(...num) {   //using rest operator 
         console.log(num)   //automatically makes it array because we use rest operator now use for loop on array and calculate total 
         let total = 0 
         for (let n of num){
