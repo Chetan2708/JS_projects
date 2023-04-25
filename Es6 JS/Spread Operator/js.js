466028887e0e9b58e622cf43679312f204096ac1
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-04-25 23:32:47 +0530

    Es6 important concepts of Js

---
+++ Es6 JS/Spread Operator/js.js
@@ -0,0 +1,30 @@
+//Spread Operator
+let a = {
+  name: 'chetan',
+  age: '18',
+  class: '6th sem',
+};
+address={
+    street:"Janpiur",
+    state:"jammu",
+}
+
+let a_copy  ={...a,...address}
+ a_copy.name = 'Copy' 
+console.log(a,a_copy)   //different name values because its a copy of a not a itself
+
+
+
+//Rest Operator
+
+function solve(...num) {
+        console.log(num)   //automatically makes it array because we use rest operator now use for loop on array and calculate total 
+        let total = 0 
+        for (let n of num){
+            total+=n
+        }
+        return total
+
+}
+
+console.log(solve(1,2,3))
