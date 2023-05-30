89d4c5c0799ebd2247c68b47d134e1b9f1932232
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-05-30 23:14:11 +0530

    Joke Generator App

--- Joke_generator/javascript.js
+++ Joke_generator/javascript.js
@@ -1,14 +1,18 @@
 const jokecontain = document.getElementById("joke");
 const button = document.getElementById("but");
-const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political&type=single"
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political&type=single";
 
-const getjoke= ()=>{
-    jokecontain.classList.remove("fade")   //animation
+// Function to fetch a joke from the API and display it
+const getjoke = () => {
+    jokecontain.classList.remove("fade"); // Remove fade animation class
     fetch(url)
-    .then(data =>data.json())
-    .then(item=> {jokecontain.innerHTML = `${item.joke}`
-    jokecontain.classList.add("fade")   //animation
-});
+        .then(data => data.json())
+        .then(item => {
+            jokecontain.innerHTML = `${item.joke}`; // Display the joke
+            jokecontain.classList.add("fade"); // Add fade animation class
+        });
 }
-button.addEventListener("click", getjoke);
-getjoke();
+
+button.addEventListener("click", getjoke); // Event listener for button click
+getjoke(); // Fetch a joke when the page loads
+
