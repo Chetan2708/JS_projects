ab3a9bbfc9e54fc23268db1cf9ca93288080c0e4
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-05-31 23:50:18 +0530

    Rock paper scissor game

--- rockpaperscissor/Rock.js
+++ rockpaperscissor/Rock.js
@@ -1,57 +1,43 @@
+// Get references to the HTML elements
 const user = document.getElementById("user");
 const comp = document.getElementById("comp");
 const result = document.getElementById("result");
-const but = document.querySelectorAll("button");
-let userchoice;
-let computerdisplay;
-let  res;
-but.forEach((but) =>
-  but.addEventListener("click", (e) => {
-    userchoice = e.target.id;
-    user.innerHTML = userchoice;
-    getrandom();
-    getresult();
+const buttons = document.querySelectorAll("button");
+
+// Add event listeners to the buttons
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    const userChoice = e.target.id;
+    user.innerHTML = userChoice;
+    const computerChoice = getRandomChoice();
+    comp.innerHTML = computerChoice;
+    const gameResult = getResult(userChoice, computerChoice);
+    result.innerHTML = gameResult;
   })
 );
-function getrandom() {
-  const random = Math.floor(Math.random() * 3);
-  console.log(random);
-  if (random === 0) {
-    computerdisplay = "rock";
-  }
-  if (random === 1) {
-    computerdisplay = "paper";
-  }
-  if (random === 2) {
-    computerdisplay = "scissor";
-  }
-  comp.innerHTML = computerdisplay;
+
+// Generate a random choice for the computer
+function getRandomChoice() {
+  const choices = ["rock", "paper", "scissor"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
 
-function getresult() {
-  if (computerdisplay === userchoice) {
-    res = "Its a draw!";
+// Determine the result of the game
+function getResult(userChoice, computerChoice) {
+  if (computerChoice === userChoice) {
+    return "It's a draw!";
   }
-  if (computerdisplay === "rock" && userchoice === "scissor") {
-    res = "Computer wins!";
+
+  // Use a switch statement for better readability
+  switch (computerChoice) {
+    case "rock":
+      return userChoice === "scissor" ? "Computer wins!" : "User wins";
+    case "paper":
+      return userChoice === "rock" ? "Computer wins!" : "User wins";
+    case "scissor":
+      return userChoice === "paper" ? "Computer wins!" : "User wins";
+    default:
+      return "Invalid choice";
   }
-  if (userchoice === "rock" && computerdisplay === "scissor") {
-    res = "User wins";
-  }
-  if (computerdisplay==="rock" && userchoice === "paper"){
-    res = "User wins";
-  }
-  if (computerdisplay === "paper" && userchoice === "scissor") {
-    res = "User wins";
-  }
-  if (computerdisplay === "paper" && userchoice === "rock") {
-    res = "Computer wins";
-  }
-  if (computerdisplay === "scissor" && userchoice === "rock") {
-    res = "User wins";
-  }
-  if (computerdisplay === "scissor" && userchoice === "paper") {
-    res = "Computer wins";
-  }
-  result.innerHTML = res
 }
