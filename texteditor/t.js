5c4aa91f62916b15160cab2b19e3ab2285c92a4d
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-06-11 23:49:48 +0530

    Rich_text_editor_app

--- texteditor/t.js
+++ texteditor/t.js
@@ -1,14 +1,25 @@
+// Get all the option buttons
 let optionsButtons = document.querySelectorAll(".option-button");
+// Get all the advanced option buttons
 let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+// Get the font name element
 let fontnameel = document.getElementById("fontName");
+// Get the font size element
 let fontSizeel = document.getElementById("fontSize");
+// Get the writing area element
 let writingArea = document.getElementById("text-input");
+// Get the create link button
 let linkButton = document.getElementById("createLink");
+// Get all the align buttons
 let alignButtons = document.querySelectorAll(".align");
+// Get all the space buttons
 let spaceButtons = document.querySelectorAll(".spacing");
+// Get all the format buttons
 let formatButtons = document.querySelectorAll(".format");
+// Get all the script buttons
 let scriptButtons = document.querySelectorAll(".script");
 
+// Define a list of fonts
 let fontList = [
     "Arial",
     "Verdana",
@@ -19,12 +30,18 @@ let fontList = [
     "Cursive",
 ];
 
+// Function to initialize the editor
 const intializer = () => {
+    // Highlight the align buttons initially
     highlighter(alignButtons, true);
+    // Highlight the space buttons initially
     highlighter(spaceButtons, true);
+    // Do not highlight the format buttons initially
     highlighter(formatButtons, false);
+    // Highlight the script buttons initially
     highlighter(scriptButtons, true);
 
+    // Add font options to the font select element
     fontList.map((value) => {
         let option = document.createElement("option");
         option.value = value;
@@ -32,6 +49,7 @@ const intializer = () => {
         fontnameel.appendChild(option);
     });
 
+    // Add font size options to the font size select element
     for (let i = 1; i <= 7; i++) {
         let option = document.createElement("option");
         option.value = i;
@@ -39,26 +57,30 @@ const intializer = () => {
         fontSizeel.appendChild(option);
     }
 
+    // Set the default font size value
     fontSizeel.value = 3;
 };
 
+// Function to modify the text based on the command
 const modifyText = (command, defaultUi, value) => {
     document.execCommand(command, defaultUi, value);
 };
 
-// basic operation that donot require any value
+// Attach click event listeners to basic option buttons
 optionsButtons.forEach((button) => {
     button.addEventListener("click", () => {
         modifyText(button.id, false, null);
     });
 });
-// operations that require change in value 
+
+// Attach change event listeners to advanced option buttons
 advancedOptionButton.forEach((button) => {
     button.addEventListener("change", () => {
         modifyText(button.id, false, button.value);
     });
 });
 
+// Attach click event listener to create link button
 linkButton.addEventListener("click", () => {
     let userLink = prompt("Enter a URL?");
     if (/http/i.test(userLink)) {
@@ -69,6 +91,7 @@ linkButton.addEventListener("click", () => {
     }
 });
 
+// Function to handle highlighting of buttons
 const highlighter = (className, needsRemoval) => {
     className.forEach((button) => {
         button.addEventListener("click", () => {
@@ -88,10 +111,12 @@ const highlighter = (className, needsRemoval) => {
     });
 };
 
+// Function to remove highlighting from buttons
 const highlighterRemover = (className) => {
     className.forEach((button) => {
         button.classList.remove("active");
     });
 };
 
+// Call the initializer function when the window loads
 window.onload = intializer();
